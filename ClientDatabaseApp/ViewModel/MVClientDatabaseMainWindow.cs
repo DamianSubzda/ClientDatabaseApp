70bed72dfc7a45edfd3afcc64290a7450195a688
DamianSubzda <DamianSubzda@wp.pl>
2024-01-31 12:51:38 +0100

    31.01.2024

--- ClientDatabaseApp/ViewModel/MVClientDatabaseMainWindow.cs
+++ ClientDatabaseApp/ViewModel/MVClientDatabaseMainWindow.cs
@@ -2,19 +2,35 @@
 using ClientDatabaseApp.Service;
 using MySqlConnector;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 
 namespace ClientDatabaseApp.ViewModel
 {
-    public class MVClientDatabaseMainWindow
+    public class MVClientDatabaseMainWindow : INotifyPropertyChanged
     {
         private MySqlConnection conn = DatabaseConnector.connection;
 
         public ICommand ShowMoreDetailsCommand { get; private set; }
         public ICommand RemoveSelectedCommand { get; private set; }
         public ICommand AddFolowUpCommand { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Client> _client;
+        public ObservableCollection<Client> Client
+        {
+            get { return _client; }
+            set
+            {
+                _client = value;
+                OnPropertyChanged(nameof(Client));
+            }
+        }
 
         public MVClientDatabaseMainWindow()
         {
@@ -24,13 +40,18 @@ namespace ClientDatabaseApp.ViewModel
             InitializeClientsGrid();
         }
 
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
         public void InitializeClientsGrid()
         {
             var context = new DBContextHVAC();
             var clients = context.ClientDBSet.ToList();
 
-            MClientDatabase.DataGridClients = clients;
+            ObservableCollection<Client> _clientsTemp = new ObservableCollection<Client>(clients);
+            Client = _clientsTemp;
         }
 
         private void ShowMoreDetails(RoutedEventArgs e)
@@ -48,63 +69,6 @@ namespace ClientDatabaseApp.ViewModel
 
         }
 
-        void AddClient()
-        {
-            try
-            {
-                conn.Open();
-
-                string insertQuery = "INSERT INTO `Client` " +
-                    "(`ClientName`, `Phonenumber`, `Email`, `City`, `Facebook`, `Instagram`, `PageURL`, `Data`, `Owner`, `Note`) " +
-                    "VALUES " +
-                    "(@ClientName, @Phonenumber, @Email, @City, @Facebook, @Instagram, @PageURL, @Data, @Owner, @Note)";
-
-                MySqlCommand cmd = new MySqlCommand(insertQuery, conn);
-                cmd.Parameters.AddWithValue("@ClientName", "Nazwa klienta");
-                cmd.Parameters.AddWithValue("@Phonenumber", "123456789");
-                cmd.Parameters.AddWithValue("@Email", "klient@example.com");
-                cmd.Parameters.AddWithValue("@City", "Miasto");
-                cmd.Parameters.AddWithValue("@Facebook", "https://www.facebook.com/klient");
-                cmd.Parameters.AddWithValue("@Instagram", "https://www.instagram.com/klient");
-                cmd.Parameters.AddWithValue("@PageURL", "https://www.example.com/klient");
-                cmd.Parameters.AddWithValue("@Data", DateTime.Now);
-                cmd.Parameters.AddWithValue("@Owner", "Właściciel");
-                cmd.Parameters.AddWithValue("@Note", "Notatka dla klienta");
-
-                cmd.ExecuteNonQuery();
-
-                Console.WriteLine("Insert wykonany pomyślnie.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Błąd: " + ex.Message);
-            }
-            finally
-            {
-                conn.Close();
-            }
-
-            //var context = new DBContextHVAC();
-
-            //Client c1 = new Client
-            //{
-            //    ClientName = "Pierwszy klient",
-            //    Phonenumber = "100100100",
-            //    Email = "@wp.pl",
-            //    City = "Wawa",
-            //    Facebook = ".pl",
-            //    Instagram = ".pl",
-            //    PageURL = ".pl",
-            //    Data = DateTime.Now,
-            //    Owner = "Ja",
-            //    Note = "notatka"
-            //};
-
-            //context.ClientDBSet.Add(c1);
-            //context.SaveChanges();
-
-
-        }
 
         void AddFollowUp()
         {
